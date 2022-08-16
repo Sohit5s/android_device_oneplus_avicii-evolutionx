481eaa5452baa8e2d6c671df0c2b13fced186c31
Sanju0910 <sreeshankar0910@gmail.com>
2022-08-16 18:30:30 +0800

    avicii: Add support for OnePlus Camera & Gallery * Apps repo: https://gitlab.com/Sanju0910/android_vendor_oneplus_apps
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,9 @@ BOARD_USES_ALSA_AUDIO := true
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := lito
 
+# Camera
+TARGET_USES_OPLUS_CAMERA := true
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
