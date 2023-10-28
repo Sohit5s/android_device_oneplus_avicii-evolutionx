199539b117a9f0fe28e3d1bc3beb28393b2d32ba
sashascurtu <sasha.scurtu@gmail.com>
2023-10-28 13:26:41 +0530

    avicii: Build mock power stats HAL Fixes the following: * Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device VINTF manifest. * BatteryStatsService: Unable to load Power.Stats.HAL. Setting rail availability to false

--- device.mk
+++ device.mk
@@ -352,6 +352,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
     
